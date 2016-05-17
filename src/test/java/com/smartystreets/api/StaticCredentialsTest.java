fdb7f3ebdd0b03257798a4b96dbbc1352ae6b74b
Oshion Niemela <oshion@smartystreets.com>
2016-05-17 15:50:20 -0600

    Extracted common behavior to helper function.

--- src/test/java/com/smartystreets/api/StaticCredentialsTest.java
+++ src/test/java/com/smartystreets/api/StaticCredentialsTest.java
@@ -6,20 +6,22 @@ import static org.junit.Assert.assertEquals;
 
 public class StaticCredentialsTest {
     @Test
-    public void testSign() throws Exception {
-        /**Case 1: normal input*/
-        StaticCredentials normalCredentials = new StaticCredentials("f83280df-s83d-f82j-d829-kd02l9tis7ek", "S9Djk63k2Ilj67vN82Km");
+    public void testStandardCredentials() throws Exception {
+        assertSignedRequest("f83280df-s83d-f82j-d829-kd02l9tis7ek", "S9Djk63k2Ilj67vN82Km",
+                "https://api.smartystreets.com/street-address?auth-id=f83280df-s83d-f82j-d829-kd02l9tis7ek&auth-token=S9Djk63k2Ilj67vN82Km");
+    }
+
+    @Test
+    public void testUrlEncoding() {
+        assertSignedRequest("as3$d8+56d9", "d8j#ds'dfe2",
+                "https://api.smartystreets.com/street-address?auth-id=as3%24d8%2B56d9&auth-token=d8j%23ds%27dfe2");
+    }
+
+    private static void assertSignedRequest(String id, String secret, String expected) {
+        StaticCredentials credentials = new StaticCredentials(id, secret);
         Request request = new Request("https://api.smartystreets.com/street-address?");
+        credentials.sign(request);
+        assertEquals(expected, request.getUrlString());
 
-        normalCredentials.sign(request);
-        String urlStringExpected = "https://api.smartystreets.com/street-address?auth-id=f83280df-s83d-f82j-d829-kd02l9tis7ek&auth-token=S9Djk63k2Ilj67vN82Km";
-        assertEquals(urlStringExpected, request.getUrlString());
-
-        /**Case 2: test for proper URL encoding*/
-        StaticCredentials weirdCredentials = new StaticCredentials("as3$d8+56d9", "d8j#ds'dfe2");
-        request = new Request("https://api.smartystreets.com/street-address?");
-        weirdCredentials.sign(request);
-        urlStringExpected = "https://api.smartystreets.com/street-address?auth-id=as3%24d8%2B56d9&auth-token=d8j%23ds%27dfe2";
-        assertEquals(urlStringExpected, request.getUrlString());
     }
 }
