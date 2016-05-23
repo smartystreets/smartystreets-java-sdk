1487c3b4e30ff6579cb4b7b9d8ffdc5b13ecda11
Neo Pimentel <neo@smartystreets.com>
2016-05-23 10:53:03 -0600

    Made ClientBuilder include a StatusCodeSender in the pipeline.

--- src/main/java/com/smartystreets/api/us_street/ClientBuilder.java
+++ src/main/java/com/smartystreets/api/us_street/ClientBuilder.java
@@ -61,6 +61,8 @@ public class ClientBuilder {
 
         Sender sender = new GoogleSender(this.maxTimeout);
 
+        sender = new StatusCodeSender(sender); //TODO: Do we need/want to make this customizable?
+
         if (this.signer != null)
             sender = new SigningSender(this.signer, sender);
 
