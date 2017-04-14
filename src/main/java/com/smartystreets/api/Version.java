18a6fc9d8f093162ed6734e82558c876f264960f
Whatcott, Michael <mdwhatcott@gmail.com>
2017-04-14 08:54:55 -0600

    Comment: Version is updated by build job.

--- src/main/java/com/smartystreets/api/Version.java
+++ src/main/java/com/smartystreets/api/Version.java
@@ -1,5 +1,5 @@
 package com.smartystreets.api;
 
 public class Version {
-    static final String CURRENT = "0.0.0";
+    static final String CURRENT = "0.0.0"; // DO NOT EDIT (this is updated by a build job when a new release is published)
 }
