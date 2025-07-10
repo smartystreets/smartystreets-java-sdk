63d0769f93758faeda47941e05f9fb3b2cdabfd7
Bryan Amundson <bryan@smartystreets.com>
2025-07-10 14:24:03 -0600

    Improved tests.

--- src/test/java/com/smartystreets/api/us_enrichment/ClientTest.java
+++ src/test/java/com/smartystreets/api/us_enrichment/ClientTest.java
@@ -9,6 +9,12 @@ import com.smartystreets.api.us_enrichment.lookup_types.property_principal.Prope
 import com.smartystreets.api.us_enrichment.result_types.georeference.GeoReferenceResponse;
 import com.smartystreets.api.us_enrichment.result_types.property_financial.FinancialResponse;
 import com.smartystreets.api.us_enrichment.result_types.property_principal.PrincipalResponse;
+import com.smartystreets.api.us_enrichment.result_types.risk.RiskResponse;
+import com.smartystreets.api.us_enrichment.lookup_types.risk.RiskLookup;
+import com.smartystreets.api.us_enrichment.result_types.secondary.SecondaryResponse;
+import com.smartystreets.api.us_enrichment.lookup_types.secondary.SecondaryLookup;
+import com.smartystreets.api.us_enrichment.result_types.secondary.SecondaryCountResponse;
+import com.smartystreets.api.us_enrichment.lookup_types.secondary.SecondaryCountLookup;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -32,9 +38,10 @@ public class ClientTest {
         URLPrefixSender sender = new URLPrefixSender("http://localhost:8080", capturingSender);
         FakeSerializer serializer = new FakeSerializer(new FinancialResponse[]{new FinancialResponse()});
         Client client = new Client(sender, serializer);
-        PropertyFinancialLookup lookup = new PropertyFinancialLookup("123", "group_structural,sale_date", "", "");
+        PropertyFinancialLookup lookup = new PropertyFinancialLookup();
+        lookup.setSmartyKey("123");
+        lookup.setInclude("group_structural,sale_date");
         client.sendPropertyFinancial(lookup);
-
         Assert.assertEquals("http://localhost:8080/123/property/financial?include=group_structural%2Csale_date", capturingSender.getRequest().getUrl());
     }
 
@@ -44,8 +51,53 @@ public class ClientTest {
         URLPrefixSender sender = new URLPrefixSender("http://localhost:8080", capturingSender);
         FakeSerializer serializer = new FakeSerializer(new GeoReferenceResponse[]{new GeoReferenceResponse()});
         Client client = new Client(sender, serializer);
-        GeoReferenceLookup lookup = new GeoReferenceLookup("123", "");
+        GeoReferenceLookup lookup = new GeoReferenceLookup("");
+        lookup.setSmartyKey("123");
         client.sendGeoReference(lookup);
         Assert.assertEquals("http://localhost:8080/123/geo-reference?", capturingSender.getRequest().getUrl());
+
+        lookup = new GeoReferenceLookup("2010");
+        lookup.setSmartyKey("123");
+        client.sendGeoReference(lookup);
+        Assert.assertEquals("http://localhost:8080/123/geo-reference/2010?", capturingSender.getRequest().getUrl());
+    }
+
+    @Test
+    public void testSendingRiskLookup() throws Exception {
+        RequestCapturingSender capturingSender = new RequestCapturingSender();
+        URLPrefixSender sender = new URLPrefixSender("http://localhost:8080", capturingSender);
+        FakeSerializer serializer = new FakeSerializer(new RiskResponse[]{new RiskResponse()});
+        Client client = new Client(sender, serializer);
+        RiskLookup lookup = new RiskLookup();
+        lookup.setSmartyKey("123");
+
+        client.sendRisk(lookup);
+        Assert.assertEquals("http://localhost:8080/123/risk?", capturingSender.getRequest().getUrl());
+    }
+
+    @Test
+    public void testSendingSecondaryLookup() throws Exception {
+        RequestCapturingSender capturingSender = new RequestCapturingSender();
+        URLPrefixSender sender = new URLPrefixSender("http://localhost:8080", capturingSender);
+        FakeSerializer serializer = new FakeSerializer(new SecondaryResponse[]{new SecondaryResponse()});
+        Client client = new Client(sender, serializer);
+        SecondaryLookup lookup = new SecondaryLookup();
+        lookup.setSmartyKey("123");
+
+        client.sendSecondary(lookup);
+        Assert.assertEquals("http://localhost:8080/123/secondary?", capturingSender.getRequest().getUrl());
+    }
+
+    @Test
+    public void testSendingSecondaryCountLookup() throws Exception {
+        RequestCapturingSender capturingSender = new RequestCapturingSender();
+        URLPrefixSender sender = new URLPrefixSender("http://localhost:8080", capturingSender);
+        FakeSerializer serializer = new FakeSerializer(new SecondaryCountResponse[]{new SecondaryCountResponse()});
+        Client client = new Client(sender, serializer);
+        SecondaryCountLookup lookup = new SecondaryCountLookup();
+        lookup.setSmartyKey("123");
+
+        client.sendSecondaryCount(lookup);
+        Assert.assertEquals("http://localhost:8080/123/secondary/count?", capturingSender.getRequest().getUrl());
     }
 }
--- src/test/java/com/smartystreets/api/us_enrichment/ResponseTest.java
+++ src/test/java/com/smartystreets/api/us_enrichment/ResponseTest.java
@@ -8,7 +8,10 @@ import com.smartystreets.api.us_enrichment.result_types.property_financial.Finan
 import com.smartystreets.api.us_enrichment.result_types.property_financial.FinancialResponse;
 import com.smartystreets.api.us_enrichment.result_types.property_principal.PrincipalAttributes;
 import com.smartystreets.api.us_enrichment.result_types.property_principal.PrincipalResponse;
+import com.smartystreets.api.us_enrichment.result_types.risk.RiskAttributes;
+import com.smartystreets.api.us_enrichment.result_types.risk.RiskResponse;
 import com.smartystreets.api.us_enrichment.result_types.secondary.*;
+import com.smartystreets.api.us_enrichment.result_types.MatchedAddress;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -24,6 +27,7 @@ public class ResponseTest {
     private static final String validSecondaryResponse = "[{\"smarty_key\":\"123\",\"root_address\":{\"secondary_count\":1,\"smarty_key\":\"root_smartykey\",\"primary_number\":\"root_primary\",\"street_predirection\":\"root_predirection\",\"street_name\":\"root_streetname\",\"street_suffix\":\"root_street_suffix\",\"street_postdirection\":\"root_postdirection\",\"city_name\":\"root_city\",\"state_abbreviation\":\"root_state\",\"zipcode\":\"root_zipcode\",\"plus4_code\":\"root_plus4\"},\"aliases\":[{\"smarty_key\":\"alias_smartykey\",\"primary_number\":\"alias_primary\",\"street_predirection\":\"alias_predirectional\",\"street_name\":\"alias_streetname\",\"street_suffix\":\"alias_streetsuffix\",\"street_postdirection\":\"alias_postdirection\",\"city_name\":\"alias_city\",\"state_abbreviation\":\"alias_state\",\"zipcode\":\"alias_zipcode\",\"plus4_code\":\"alias_plus4\"}],\"secondaries\":[{\"smarty_key\":\"secondary_smartykey\",\"secondary_designator\":\"secondary_designator\",\"secondary_number\":\"secondary_number\",\"plus4_code\":\"secondary_plus4\"}]}]";
     private static final String validSecondaryCountResponse = "[{\"smarty_key\":\"123\",\"count\":10}]";
     private static final String validGeoReferenceResponse = "[{\"smarty_key\":\"123\",\"data_set_name\":\"geo-reference\",\"etag\":\"ABCD\",\"attributes\":{\"census_block\":{\"accuracy\":\"block\",\"geoid\":\"180759630002012\"},\"census_county_division\":{\"accuracy\":\"exact\",\"code\":\"1807581764\",\"name\":\"Wayne\"},\"place\":{\"accuracy\":\"exact\",\"code\":\"1861236\",\"name\":\"Portland\",\"type\":\"incorporated\"}}}]";
+    private static final String validRiskResponse = "[{\"smarty_key\":\"123\",\"data_set_name\":\"risk\",\"etag\":\"ABCD\",\"matched_address\":{\"street\":\"street value\"},\"attributes\":{\"AGRIVALUE\":\"agrivalue\",\"AREA\":\"area\",\"NRI_VER\":\"nri_ver\"}}]";
 
     @Test
     public void testPrincipalFieldValues() throws IOException {
@@ -113,7 +117,7 @@ public class ResponseTest {
     }
 
     @Test
-    public void testSecondaryCountFiedlValues() throws IOException {
+    public void testSecondaryCountFieldValues() throws IOException {
         SecondaryCountResponse[] results = smartySerializer.deserialize(validSecondaryCountResponse.getBytes(), SecondaryCountResponse[].class);
         SecondaryCountResponse result = results[0];
 
@@ -122,7 +126,7 @@ public class ResponseTest {
     }
 
     @Test
-    public void testGeoReferenceFieldsGetFilledInCorrectly() throws IOException {
+    public void testGeoReferenceFieldValues() throws IOException {
         GeoReferenceResponse[] results = smartySerializer.deserialize(validGeoReferenceResponse.getBytes(), GeoReferenceResponse[].class);
 
         GeoReferenceResponse result = results[0];
@@ -149,6 +153,24 @@ public class ResponseTest {
         assertEquals("1861236", attributes.place.code);
         assertEquals("Portland", attributes.place.name);
         assertEquals("incorporated", attributes.place.type);
+    }
+
+    @Test
+    public void testRiskFieldValues() throws IOException {
+        RiskResponse[] results = smartySerializer.deserialize(validRiskResponse.getBytes(), RiskResponse[].class);
+
+        RiskResponse result = results[0];
+        assertEquals("123", result.getSmartyKey());
+        assertEquals("ABCD", result.getEtag());
+
+        MatchedAddress matchedAddress = result.getMatchedAddress();
+        assertEquals("street value", matchedAddress.street);
+
+        assertNotNull(result.getAttributes());
+        RiskAttributes attributes = result.getAttributes();
+        assertEquals("agrivalue", attributes.AGRIVALUE);
+        assertEquals("area", attributes.AREA);
+        assertEquals("nri_ver", attributes.NRI_VER);
     }
 
 }
