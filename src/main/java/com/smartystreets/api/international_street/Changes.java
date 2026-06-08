7134a524ac1ebe946241f204331fc4ee84f7d1fb
Andy Johnson <andrew.johnson@smarty.com>
2026-06-08 15:44:36 -0600

    Sync international street candidate fields

--- src/main/java/com/smartystreets/api/international_street/Changes.java
+++ src/main/java/com/smartystreets/api/international_street/Changes.java
@@ -6,11 +6,16 @@ public class Changes extends RootLevel implements Serializable {
 
     //region [ Fields ]
 
+    private String country;
     private Components components;
 
     //endregion
 
     //region [ Getters ]
+
+    public String getCountry() {
+        return country;
+    }
 
     public Components getComponents() {
         return components;
